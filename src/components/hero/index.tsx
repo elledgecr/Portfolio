b9d5b95210d5aba5fa6569afa4bbb5efd3dc136c
Chris <33050240+Cigatronix@users.noreply.github.com>
2024-11-07 22:47:16 -0700

    About/Work Experience Feature (#6)
    * Correct NavBar utilization of Functional Components
    
    * Add Work Experience Information

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -8,7 +8,7 @@ const Hero: React.FC = () => {
   return (
     <section
       id="hero"
-      className="bg-darkOne text-white flex flex-col md:flex-row items-center justify-center min-h-screen py-20 px-8 space-y-10 md:space-y-0 md:space-x-12"
+      className="bg-darkOne text-white flex flex-col md:flex-row items-center justify-center py-40 px-8 space-y-10 md:space-y-0 md:space-x-12"
     >
       <div className="flex flex-col items-center mb-8 md:mb-0">
         <HeroPictureDisplay />
@@ -20,12 +20,12 @@ const Hero: React.FC = () => {
 
         <div className="flex flex-col space-y-2 text-gray-400 mb-4">
           <HeroContactItem
-            label="Phone:"
+            label="Phone"
             value="+1 (720)-839-3268"
             href="tel:+17208393268"
           />
           <HeroContactItem
-            label="Email:"
+            label="Email"
             value="elledgecre@outlook.com"
             href="mailto:elledgecre@outlook.com"
           />
